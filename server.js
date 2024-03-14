81260268292cc94ce064cbcceebd21565cb6e5b8
KNProNooob <knpronoob@gmail.com>
2024-03-14 19:55:38 +0200

    Working get method for the thingy

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 const express  = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 const port = 80
 app.use(express.static('views'));
 app.set('view engine', 'ejs') 
+app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
     res.render('index')
 })
 
-app.get('/test', (req, res) => {
-    console.log(req.body)
+app.get('/test', async (req, res) => {
+    console.log(req)
     console.log(req.query)
     res.send("Never Gonna Give You Up")
 })
