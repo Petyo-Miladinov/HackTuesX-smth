3c47d37206dc1ccde5af23d405c165fdd902d1cf
KNProNooob <knpronoob@gmail.com>
2024-03-15 14:08:41 +0200

    commit

--- server.js
+++ server.js
@@ -3,26 +3,47 @@ const bodyParser = require('body-parser')
 const app = express()
 const fs = require('fs')
 
+
 const port = 80
 app.use(express.static('views'));
 app.set('view engine', 'ejs') 
 app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
-    res.render('index')
+    res.render('account')
 })
 
 app.get('/add', async (req, res) => {
-    let data = req.query.data
-    console.log(data)
-    let coods = data.split(",")
-    data = "v " + coods[0] + " " + coods[1] + " " + coods[2] + "\n"
-    fs.appendFile("3d/model.obj", data, (err) =>{})
-    res.send("Received")
+    
+    if(req.query.id == null){
+        res.send("no id. f u")
+    }
+    else{
+        console.log(req.query)
+        let data = req.query.data
+        data = data + "\n"
+        fs.appendFile("3d/" + req.query.id + "/model.txt", data, (err) =>{})
+        res.send("Received")
+    }
+})
+
+app.get('/new_file', async(req, res) => {
+    console.log("aaa")
+    if(req.query.id == null){
+        res.send("no id. f u")
+    }
+    else{
+        console.log(fs.existsSync("3d/" + req.query.id))
+        if(fs.existsSync(__dirname + "/3d/" + req.query.id) == false){
+            fs.mkdir("3d/" + req.query.id, (err) => {})
+        }
+        fs.writeFile("3d/" + req.query.id + "model.txt", "", (err) =>{})
+        res.send("Done")
+    }
 })
 
 app.get('/download', (req, res) =>{
-    res.download(__dirname + "/3d/model.obj")
+    res.download(__dirname + "/3d/model.txt")
 })
 
 app.listen(port, () => {
